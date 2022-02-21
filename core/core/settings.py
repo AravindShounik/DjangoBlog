87b73c78626aff024adfad4d87b6264190d2b5fd
AravindShounik <vangaaravindshounik@gmail.com>
2022-02-21 12:49:36 +0530

    updated settings.py and added a requrements.txt file

--- core/core/settings.py
+++ core/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'Blog-Home'
 
 LOGIN_URL = 'login'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
